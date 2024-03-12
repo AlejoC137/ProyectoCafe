959492556994abc88c79de6881b1bf3e2a5c5b49
AlejoC137 <dapatinoa@unal.edu.co>
2024-03-12 16:51:12 -0500

    11-test git pages

--- FRONT/vite.config.js
+++ FRONT/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://<AlejoC137>.github.io/Portafolio_alejoc137'
 })
