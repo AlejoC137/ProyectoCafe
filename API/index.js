d9d2fefd967d6788f9e890d2533fb930b79c7e17
AlejoC137 <dapatinoa@unal.edu.co>
2024-03-16 22:40:20 -0500

    12.5 test deploy API

--- API/index.js
+++ API/index.js
@@ -1,9 +1,9 @@
 const server = require("./src/app.js")
-const {run} = require('./src/dataBase/dataBase.js')
+// const {run} = require('./src/dataBase/dataBase.js')
 
 const PORT = 3001
 server.listen(PORT , ()=>{
-    run();
+    // run();
     console.log(`Listening on port ${PORT}`)
     
 
