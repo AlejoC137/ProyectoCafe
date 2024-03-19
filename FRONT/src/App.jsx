2b5db2c1989ae9013b15e295af9ab2df1fa31a84
AlejoC137 <dapatinoa@unal.edu.co>
2024-03-18 21:52:49 -0500

    12.1.1  test deploy CLI

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
     <Routes>
 
+        <Route path="/" element={<Home/>} />
         <Route path="/home" element={<Home/>} />
         <Route path="/wifiportal" element={<WifiPortal/>} />
         <Route path="/newproject" element={<NewProjectPortal/>} />
